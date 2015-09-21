c69516eb3c69447adc462bb5f0100d1383d07c49
Renzo D Santillan Ch <rsantillanc@gmail.com>
2015-09-21 18:23:59 -0500

    Structure proyect rename part2

---
+++ app/src/main/java/rsantillanc/aboutme/Persistance/SP/LoginSP.java
@@ -0,0 +1,7 @@
+package rsantillanc.aboutme.Persistance.SP;
+
+/**
+ * Created by rsantillanc on 21/09/2015.
+ */
+public class LoginSP {
+}
---
+++ app/src/main/java/rsantillanc/aboutme/Util/Const.java
@@ -0,0 +1,7 @@
+package rsantillanc.aboutme.Util;
+
+/**
+ * Created by rsantillanc on 21/09/2015.
+ */
+public class Const {
+}
